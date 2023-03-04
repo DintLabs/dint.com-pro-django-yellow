34c3e5f8c53ffc9ebdeb6d5ceb559844851ff5ec
Max <info@dint.com>
2023-03-04 18:23:59 -0500

    Update settings.py

--- dint/settings.py
+++ dint/settings.py
@@ -29,7 +29,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-ALLOWED_HOSTS = ['127.0.0.1', config('WALLET_URL_DINT'), config('BE_URL_DINT')]
+ALLOWED_HOSTS = ['127.0.0.1', config('DINT'), config('WALLET_URL_DINT'), config('BE_URL_DINT')]
 
 # Application definition
 INSTALLED_APPS = [
